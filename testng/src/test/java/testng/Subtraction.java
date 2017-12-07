dff637cd93edb4086ef325b91c75749a379c6666
Ashwin Gudepu <Ashwin_Gudepu@epam.com>
2017-12-07 20:54:53 +0530

    Added Subtraction Tests

--- testng/src/test/java/testng/Subtraction.java
+++ testng/src/test/java/testng/Subtraction.java
@@ -1,5 +1,54 @@
 package testng;
 
-public class Subtraction extends BaseCalculator {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import com.epam.tat.module4.Calculator;
+
+public class Subtraction extends BaseCalculator {
+	
+	@BeforeSuite
+	public void setUp(){
+		calculator=new Calculator();
+		System.out.println("Before Suite");
+	}
+
+	@Test(dataProvider = "subtractingFunctionality")
+	public void subtractionOfTwoNumbers(long firstNumber, long secondNumber, long expected) {
+		long result = calculator.sub(firstNumber, secondNumber);
+		Assert.assertEquals(result, expected);
+	}
+
+	@DataProvider(name = "subtractingFunctionality")
+	public Object[][] dataprovider() {
+		return new Object[][] { { 9, 3, 6 } };
+	}
+	
+	
+	@Test
+	public void subTwoNumbers() {
+		long result = calculator.sub(10, 5);
+		Assert.assertEquals(result, 5);
+	}
+	
+	@Test
+	public void subTwoNegativeNumbers() {
+		long result = calculator.sub(-10, -5);
+		Assert.assertEquals(result, -5);
+	}
+	
+	@Test
+	public void subOneNegativeAndOnePositiveNumber() {
+		long result = calculator.sub(-10, 5);
+		Assert.assertEquals(result, -15);
+	}
+	
+	@Test
+	public void subOnePositiveAndOneNegativeNumber() {
+		long result = calculator.sub(10, -5);
+		Assert.assertEquals(result, 15);
+	}
 }
