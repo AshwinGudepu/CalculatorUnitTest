bd5745f90e2de8299acad18a6c898b290f7914c3
Ashwin Gudepu <Ashwin_Gudepu@epam.com>
2017-12-10 21:36:33 +0530

    Updated parallelTestNG.xml and parallel tests

--- testng/src/test/java/testng/CtgTest.java
+++ testng/src/test/java/testng/CtgTest.java
@@ -14,35 +14,35 @@ public class CtgTest extends BaseCalculator {
 	}
 	
 	@Test
-	public void ctgZero() {
+	public void ctg0() {
 		double result = calculator.ctg(0);
 		Assert.assertEquals(result, 0.0);
 		//This test is failed. Ideally value is not defined. As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void ctgThirty() {
+	public void ctg30() {
 		double result = calculator.ctg(30);
 		Assert.assertEquals(result, 1.73205081);
 		//This test is failed. Ideally value is not defined. As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void ctgFourtyFive() {
+	public void ctg45() {
 		double result = calculator.ctg(45);
 		Assert.assertEquals(result, 1.0);
 		//This test is failed. Ideally value is not defined. As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void ctgSixty() {
+	public void ctg60() {
 		double result = calculator.ctg(60);
 		Assert.assertEquals(result, 0.57735027);
 		//This test is failed. Ideally value is not defined. As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void ctgNinety() {
+	public void ctg90() {
 		double result = calculator.ctg(90);
 		Assert.assertEquals(result, 0.0);
 		//This test is failed. Ideally value is not defined. As part of assignment I am writing these comments
--- testng/src/test/java/testng/TanTest.java
+++ testng/src/test/java/testng/TanTest.java
@@ -7,34 +7,34 @@ import static org.testng.Assert.assertEquals;
 public class TanTest extends BaseCalculator {
 	
 	@Test
-	public void tanZero() {
+	public void tan0() {
 		double result = calculator.tg(0);
 		Assert.assertEquals(result, 0.0);
 		//This test is failed. Expected Value is 0, but returning NaN.As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void tgThirty() {
+	public void tg30() {
 		double result = calculator.tg(30);
 		Assert.assertEquals(result, 0.57735026919);
 		//This test is failed. Expected Result is not matching with the Actual Result. As part of assignment I am writing these comments
 	}
 
 	@Test
-	public void tgFourtyFive() {
+	public void tg45() {
 		double result = calculator.tg(45);
 		Assert.assertEquals(result, 1.0);		
 	}
 
 	@Test
-	public void tgSixty() {
+	public void tg60() {
 		double result = calculator.tg(60);
 		Assert.assertEquals(result, 1.732050807569);
 		//This test is failed. Expected Result is not matching with the Actual Result. As part of assignment I am writing these comments
 	}
 
 	@Test	
-	public void tgNinety() {
+	public void tg90() {
 		double result = calculator.tg(90);
 		if(Double.isNaN(result)){
 		assertEquals(true, true);
